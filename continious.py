2456063ba354108a9e7cefc34fad492d50167c29
misterion <Ilyalas@gmail.com>
2018-05-20 01:19:46 +0300

    Fixed interval frequencies counter

--- continious.py
+++ continious.py
@@ -5,6 +5,8 @@ class ContiniousVariable(Variable):
     def __init__(self, datalist):
         super().__init__(datalist)
 
+        self.range = max(self.data_set) - min(self.data_set)
+
         self.count_expectation()
         self.count_variance()
         self.count_deviation()
@@ -13,26 +15,30 @@ class ContiniousVariable(Variable):
         print(self.variance)
         print(self.standart_deviation)
 
+
+
     def get_delta(self):
         STURGIS_COEFFICIENT = 3.322
-        return (max(self.data_set) - min(self.data_set)) / (1 + STURGIS_COEFFICIENT * math.log10(self.datalist_len))
+        return self.range / (1 + STURGIS_COEFFICIENT * math.log10(self.datalist_len))
 
 
     def get_intervals(self):
         intervals = []
         delta = self.get_delta()
         left = min(self.data_set)
-        first = left
+
+        i = 0
+        frequency_sum = 0
         for value in self.data_set:
             if value > left + delta:
-                frequency_sum = sum(self.frequencies[self.data_set.index(first):self.data_set.index(value) - 2])
                 intervals.append((left, left + delta, frequency_sum))
                 left += delta
-                first = value
+                frequency_sum = 0
 
-        frequency_sum = sum(self.frequencies[self.data_set.index(first):-1])
-        intervals.append((left, left+delta,frequency_sum))
+            frequency_sum += self.frequencies[i]
+            i += 1
 
+        intervals.append((left, left+delta, frequency_sum))
         return intervals
 
 
